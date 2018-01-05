23bbf88fd4038d541227ec0e8c901c329466d506
Rafael Coelho <rafaellcoellho@gmail.com>
2018-01-05 16:53:04 -0300

    chore(scripts): download just the checkpatch and not the spellcheck.txt file

--- scripts/download.sh
+++ scripts/download.sh
@@ -5,13 +5,11 @@
 # Use this script to fetch the latest checkpatch.pl from GitHub
 
 readonly CHECKPATCH_SCRIPT="checkpatch.pl"
-readonly SPELLING_TXT="spelling.txt"
 
 readonly KERNEL_RAW_URL="https://raw.githubusercontent.com/torvalds/linux/master"
 readonly CHECKPATCH_URL="${KERNEL_RAW_URL}/scripts/${CHECKPATCH_SCRIPT}"
-readonly SPELLING_URL="${KERNEL_RAW_URL}/scripts/${SPELLING_TXT}"
 
-for download in "${CHECKPATCH_URL}:${CHECKPATCH_SCRIPT}" "${SPELLING_URL}:${SPELLING_TXT}"; do
+for download in "${CHECKPATCH_URL}:${CHECKPATCH_SCRIPT}"; do
 	echo "Downloading '${download##*:}'..."
 	curl -f "${download%:*}" -s -S -O || \
 		exit 1
