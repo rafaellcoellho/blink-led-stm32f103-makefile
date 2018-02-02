e61ecb30401441cc020356add78226c461303d78
Rafael Coelho <rafaellcoellho@gmail.com>
2018-02-02 10:13:10 -0300

    Not use checkpath anymore, use uncrustify

--- MakeAnalysis.mk
+++ MakeAnalysis.mk
@@ -1,7 +1,4 @@
-# .c files
-C_SOURCE=$(wildcard ./src/*.c)
-
-all: folder sources
+all: folder
 
 folder: $(C_SOURCE)
 	@ echo 'Running static analysis with cppcheck in the project...'
@@ -9,10 +6,3 @@ folder: $(C_SOURCE)
 	cppcheck ./src/ ./src_stm32/
 	@ echo ' '
 	@ echo 'Finished'
-
-
-sources: $(C_SOURCE)
-	@ echo 'Running checkpatch in source and header files...'
-	./scripts/checkpatch.pl --no-tree -f ./src/*.c ./src/*.h ./src_stm32/*.c ./src_stm32/*.h
-	@ echo 'Finished'
-	@ echo ' '
